0f5fab01d3db19f5be2b5abaf2153c4d3cc69481
vicmaburrito <vmaldaraca@gmail.com>
2021-12-08 15:08:24 -0600

    Complete Calculator test

--- calculator/caculator.test.js
+++ calculator/caculator.test.js
@@ -25,3 +25,27 @@ describe('subtract',() => {
         expect(calc.subtract(28-7)).toBe(21);
     })
 })
+
+describe('multiply',() => {
+    it('The multiply 2*2 should to be equal with 2',() => {
+        expect(calc.multiply(2,2)).toBe(4);
+    })
+    it('The multiply 3*3 should to be equal with 9',() => {
+        expect(calc.multiply(3,3)).toBe(9);
+    })
+    it('The multiply 4*4 should to be equal with 30',() => {
+        expect(calc.multiply(4,4)).toBe(16);
+    })
+})
+
+describe('divide',() => {
+    it('The division 1/1 should to be equal with 1',() => {
+        expect(calc.divide(1,1)).toBe(1);
+    })
+    it('The division (6/3)/2 should to be equal with 1',() => {
+        expect(calc.divide(6,3,2)).toBe(1);
+    })
+    it('The division 25/5 should to be equal with 5',() => {
+        expect(calc.divide(25,5)).toBe(5);
+    })
+}) 
